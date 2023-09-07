9d7e7f2b5d5dbf05715a483b1042375a985c94e5
Abiorh001 <abiolaadedayo1993@gmail.com>
2023-09-07 19:31:22 +0100

    stage one

--- zuri_apis/urls.py
+++ zuri_apis/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include('zuri_endpoints.urls'))
+    path('api', include('zuri_endpoints.urls'))
 ]
